6ed78495671ece28ca8e746a7d4a9df24d119da2
Stephen Pope <github@stephenpope.co.uk>
2018-08-21 22:43:58 +0200

    Fix docker permissions.

--- install/docker/install.sh
+++ install/docker/install.sh
@@ -17,11 +17,11 @@ if ! type "docker" > /dev/null; then
 
     sudo systemctl status docker
 
-    curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
     sudo chmod +x /usr/local/bin/docker-compose
 
-    curl -L https://github.com/docker/machine/releases/download/v0.14.0/docker-machine-`uname -s`-`uname -m` -o /usr/local/bin/docker-machine 
+    sudo curl -L https://github.com/docker/machine/releases/download/v0.14.0/docker-machine-`uname -s`-`uname -m` -o /usr/local/bin/docker-machine 
 
     sudo chmod +x /usr/local/bin/docker-machine
 
