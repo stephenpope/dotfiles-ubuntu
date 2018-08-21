93e2b5686c5cfd831632f1e57b00a0619b416dec
Stephen Pope <github@stephenpope.co.uk>
2018-08-21 22:04:31 +0200

    Clean up downloaded files.

--- install/dotnet/install.sh
+++ install/dotnet/install.sh
@@ -8,6 +8,7 @@ if ! type "dotnet" > /dev/null; then
 
     wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
+    rm -f packages-microsoft-prod.deb
 
     sudo apt-get -y install apt-transport-https
     sudo apt-get update
