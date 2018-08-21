0294438b5a5ecde24351b3835f274908a3ca38ba
Stephen Pope <github@stephenpope.co.uk>
2018-08-21 22:46:57 +0200

    Remove unwanted output.

--- install/docker/install.sh
+++ install/docker/install.sh
@@ -15,8 +15,6 @@ if ! type "docker" > /dev/null; then
 
     sudo apt install -y docker-ce
 
-    sudo systemctl status docker
-
     sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
     sudo chmod +x /usr/local/bin/docker-compose
