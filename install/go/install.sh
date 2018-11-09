3cc606bbcfbaf9cce6bb30c11e9c2fc616ea0ecf
Stephen Pope <github@stephenpope.co.uk>
2018-11-09 13:54:23 +0000

    Extra go instructions.

--- install/go/install.sh
+++ install/go/install.sh
@@ -3,10 +3,11 @@
 if ! type "go" > /dev/null; then
     
     echo "-----------"
-    echo "INSTALL : go"
+    echo "INSTALL : go (1.11.2)"
     echo "-----------"
-    sudo apt -y install snapd
-    sudo snap install --classic go
+    sudo wget https://dl.google.com/go/go1.11.2.linux-amd64.tar.gz
+    sudo tar -xvf go1.11.linux-amd64.tar.gz
+    sudo mv go /usr/local
 
     echo "-----------"
 fi
