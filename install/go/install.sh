03cda241153f36b887d51d1de437152788597c76
Stephen Pope <github@stephenpope.co.uk>
2018-11-14 12:00:56 +0000

    Fix GO installation.

--- install/go/install.sh
+++ install/go/install.sh
@@ -6,7 +6,7 @@ if ! type "go" > /dev/null; then
     echo "INSTALL : go (1.11.2)"
     echo "-----------"
     sudo wget https://dl.google.com/go/go1.11.2.linux-amd64.tar.gz
-    sudo tar -xvf go1.11.linux-amd64.tar.gz
+    sudo tar -xvf go1.11.2.linux-amd64.tar.gz
     sudo mv go /usr/local
 
     echo "-----------"
